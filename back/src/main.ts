909e1665584e3201b7f161449e5b6f8b78ccdaca
AngelQ0314 <leonelquishpe0314@gmail.com>
2026-01-26 08:46:17 -0500

    docker

--- back/src/main.ts
+++ back/src/main.ts
@@ -5,9 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: ['http://localhost:4200', 'http://localhost:8080', 'http://localhost:80'],
     methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
   });
 
   await app.listen(process.env.PORT ?? 3000);
